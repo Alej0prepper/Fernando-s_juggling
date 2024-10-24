a62db8ea1fdbb0c2d065faf2fb512e6ab0209f39
frankperez-github <fp848584@gmail.com>
2024-10-24 14:02:34 -0400

    fix/make open visual an option

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import argparse
 from graph import Graph
-from scraper import input_siteswap_and_click
+
 from parser import parse_siteswap
+
+parser = argparse.ArgumentParser(description='Process siteswap.')
+parser.add_argument('mode', nargs='?', default='visual', help='Mode to run the script: "visual" or "no-visual"')
+args = parser.parse_args()
 
 siteswap = input("Enter the siteswap you want to learn: ")
 
-# Example usage:
 graph = Graph()
 graph.build_graph(parse_siteswap(siteswap), siteswap)
 subsiteswap_list = graph.get_subsiteswap_list()
@@ -12,4 +16,10 @@ subsiteswap_list.reverse()
 siteswaps = []
 for ss in subsiteswap_list:
     siteswaps.append(''.join([str(value) for value in ss]))
-input_siteswap_and_click(siteswaps, "ss", "input[type='submit'][value='Go']")
+print(ss)
+
+if args.mode != 'no-visual':
+    from scraper import input_siteswap_and_click
+    
+if args.mode != 'no-visual':
+    input_siteswap_and_click(siteswaps, "ss", "input[type='submit'][value='Go']")
